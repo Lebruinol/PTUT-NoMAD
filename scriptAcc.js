040464d149007443520ea55740367165307ff935
Lebruinol <72467121+Lebruinol@users.noreply.github.com>
2021-12-21 14:42:43 +0100

    Update scriptAcc.js

--- scriptAcc.js
+++ scriptAcc.js
@@ -1,1 +1,7 @@
 alert("script ajouté");
+main() {
+  var divNavPrese = dom.getElementById("navPrese");
+  divNavPrese.onclick = "window.location.href='https://github.com/Lebruinol/PTUT-NoMAD/pages/team/'";
+  alert("onclick Prese set");
+  
+}
