e17ea2fe9adddb4b401193e80454e900839a5ff8
Lebruinol <72467121+Lebruinol@users.noreply.github.com>
2021-12-21 15:05:43 +0100

    Update scriptAcc.js

--- scriptAcc.js
+++ scriptAcc.js
@@ -1,6 +1,3 @@
 alert("script ajouté");
 
-var divNavPrese = document.getElementById("navPrese");
-divNavPrese.onclick = "window.location.href='https://github.com/Lebruinol/PTUT-NoMAD/pages/team/'";
-alert("onclick Prese set");
-  
+
