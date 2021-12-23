2789378bc0add1097d3a8bf880e15e39a833c994
Lebruinol <72467121+Lebruinol@users.noreply.github.com>
2021-12-23 21:59:46 +0100

    MaJ Carrousel
    MaJ carrousel vers la branche main

--- scriptAcc.js
+++ scriptAcc.js
@@ -1,2 +1,31 @@
+//script de la page d'acceuil
 
 
+var carrouselWN = document.getElementById("whatsnew");
+var carrouselWNG = document.getElementById("wnSlideGauche");
+var carrouselWND = document.getElementById("wnSlideDroite");
+
+
+//setup des events
+carrouselWNG.onclick = carrousselSlideGauche();
+
+carrouselWN.onclick = console.log("SLIDE");
+
+
+
+
+//var carrouselWNBackgroundImg = carrouselWN.getAttribute("background");
+
+carrouselWN.setAttribute("background", "url(img/oralPres.jpg)");
+
+
+
+
+function carrousselSlideGauche() {
+	console.log("Carrousel slide gauche");
+	carrouselWN.setAttribute("background", "url(img/oralPres.jpg)");
+	
+	//mise a jour du text et du titre
+	
+	//faire la maitrise de la transition
+}
